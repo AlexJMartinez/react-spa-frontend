ee375235a2238bed42ca417d3cb29c581ed7ece1
AlexJMartinez <alexandr3martin3z@gmail.com>
2021-07-27 18:13:25 -0700

    added text and link to backend repo in the readme.md file

--- src/components/About.js
+++ src/components/About.js
@@ -9,9 +9,9 @@ const About = () => {
             <p>This app is meant to be for anonymous photo sharing.
             There are no profiles, no users, no DM functionality, it is for the sole
             purpose of posting whatever you'd like, uncensored or restricted. There 
-            is though, the ability to like photos and a page to display our top 10
-            liked photos to give visitors to the site incentive to post what they 
-            deem to be worthy of one of those top 10 spots.</p>
+            will be soon though, the ability to like photos, comment and a page to display our top 10
+            liked photos (currently labeled "recent") to give visitors to the site incentive 
+            to post what they deem to be worthy of one of those top 10 spots.</p>
         </div>
     )
 }
